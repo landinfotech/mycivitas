de725a9ac2e1830c14bab8005b0b863492e75122
Jeff Osundwa <jeff@kartoza.com>
2024-02-06 18:26:06 +0300

    test: add tests on community map(assests and tickets)

--- playwright/staging-tests/playwright.config.ts
+++ playwright/staging-tests/playwright.config.ts
@@ -27,6 +27,8 @@ export default defineConfig({
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    // Viewport used for all pages in the context.
+    viewport: { width: 1280, height: 720 },
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: 'https://staging.mycivitas.ca/',
 
